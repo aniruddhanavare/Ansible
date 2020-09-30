9c542f73cd11915b3b6309f350ed28405248c0f1
Aniruddha Navare <vareanan@in.ibm.com>
2020-09-30 17:23:33 +0530

    Added new Playbooks

---
+++ Services/LAMP/files/index.php
@@ -0,0 +1,9 @@
+<html>
+   <head>
+      <title>MCMP Sales Engineering PHP Sample</title>
+      <meta http-equiv=\Content-Type\ content=\text/html; charset=ISO-8859-1\>
+   </head>
+   <body>
+      <h1>Welcome to the MCMP Sales Engineering PHP Sample</h1>
+   </body>
+</html>
