4f05cf442930fd2871cf128d869b9e5caeff5824
sonalkakode <sonalkakode@gmail.com>
2021-06-18 23:01:15 +0530

    Update configure_master_vmdb.sh

--- vmbased_configurations_sh/configurations/configure_master_vmdb.sh
+++ vmbased_configurations_sh/configurations/configure_master_vmdb.sh
@@ -64,6 +64,7 @@ appliance_console_cli \
     --password=$db_pass \
     --auto-failover
 echo $?
+reboot
 echo `date` "Task: Configure database replication : COMPLETE"
 
 # Database replication status
