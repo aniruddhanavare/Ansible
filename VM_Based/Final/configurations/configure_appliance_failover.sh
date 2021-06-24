f3bad778f188cf460d43eabe4da44961f0422ac3
sonal <sonkakod@in.ibm.com>
2021-06-24 13:34:41 +0530

    Fixed authorization policy ssh and service issues

--- VM_Based/Final/configurations/configure_appliance_failover.sh
+++ VM_Based/Final/configurations/configure_appliance_failover.sh
@@ -4,5 +4,6 @@ echo `date` "== CONFIGURE UI APPLIANCE [$miq_hostname] FAILOVER : START =="
 
 # Start evm failover monitor
 echo `date` "Task : Start evm failover monitor : START"
+systemctl enable evm-failover-monitor.service
 systemctl start evm-failover-monitor.service
 echo `date` "Task : Start evm failover monitor : COMPLETE"
